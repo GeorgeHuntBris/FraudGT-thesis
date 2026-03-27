fb28aabf231f3cde23bb7c2ca4e900c95529f607
George Hunt <georgehunt@Georges-MacBook-Air.local>
2026-03-27 11:02:37 +0000

    Add auto_resume and increase time limit to 24h for ETH-Aux-Multi

--- run/eth_baselines/run_eth_aux_multi.sh
+++ run/eth_baselines/run_eth_aux_multi.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=24G
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 
